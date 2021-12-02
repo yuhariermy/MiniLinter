0432d6b5426590fc0ef123783804d97a11eaacac
yuhariermy <yuhariermy@gmail.com>
2021-12-02 18:10:11 +0700

    Number Guesser Finished

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -3,3 +3,27 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+  return (randomNumber = Math.floor(Math.random() * 9));
+};
+
+const compareGuesses = (humanGuesses, computerGuesses, secretTargetNumber) => {
+  let human = Math.abs(secretTargetNumber - humanGuesses);
+  let computer = Math.abs(secretTargetNumber - computerGuesses);
+  return (result = human > computer ? true : false);
+};
+
+const updateScore = (winner) => {
+  return (result =
+    winner === "human" ? (humanScore += 1) : (computerScore += 1));
+};
+
+const advanceRound = () => {
+  return (currentNumber += 1);
+};

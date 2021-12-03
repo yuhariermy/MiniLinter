7cbf14af894922f7469a10a5e31b7c55126d569e
yuhariermy <yuhariermy@gmail.com>
2021-12-03 09:45:49 +0700

    implement .word(), .pad() and .has() method

--- Lodash/main.js
+++ Lodash/main.js
@@ -23,6 +23,35 @@ const _ = {
       return false;
     }
   },
+
+  words(theString) {
+    return theString.split(" ");
+  },
+
+  pad(string, length) {
+    if (length <= string.length) {
+      return string;
+    } else {
+      const theSpace = " ";
+
+      const difference = length - string.length;
+      const spaceExtra = difference % 2;
+      const spaceCount = Math.floor(difference / 2);
+      return (
+        theSpace.repeat(spaceCount) +
+        string +
+        theSpace.repeat(spaceCount + spaceExtra)
+      );
+    }
+  },
+
+  has(object, key) {
+    if (typeof object[key] === "undefined") {
+      return false;
+    } else {
+      return true;
+    }
+  },
 };
 
 // Do not write or modify code below this line.

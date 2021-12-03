7b2e503fd2f4f8457602e49243d58629d67c9c98
yuhariermy <yuhariermy@gmail.com>
2021-12-03 09:44:15 +0700

    implement .inRange() method

--- Lodash/main.js
+++ Lodash/main.js
@@ -4,6 +4,25 @@ const _ = {
     const clampedValue = Math.min(lowerClampedValue, upper);
     return clampedValue;
   },
+
+  inRange(number, startVal, endVal) {
+    if (typeof endVal === "undefined") {
+      endVal = startVal;
+      startVal = 0;
+    }
+
+    if (startVal > endVal) {
+      let valueSwap = endVal;
+      endVal = startVal;
+      startVal = valueSwap;
+    }
+
+    if (number >= startVal && number <= endVal) {
+      return true;
+    } else {
+      return false;
+    }
+  },
 };
 
 // Do not write or modify code below this line.

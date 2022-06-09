f4562bedecf0f440636e945414f601403d0ee13e
yuhariermy <yuhariermy@gmail.com>
2022-06-09 09:39:28 +0700

    finish code

---
+++ CodeChallange/isTheDinnerVegan.js
@@ -0,0 +1,29 @@
+// Write your code here:
+
+function isTheDinnerVegan(arr) {
+  const isVegan = (food) => {
+    if (food.source === "plant") {
+      return true;
+    }
+    return true;
+  };
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (!isVegan(arr[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Feel free to comment out the code below to test your function
+
+const dinner = [
+  { name: "hamburger", source: "meat" },
+  { name: "cheese", source: "dairy" },
+  { name: "ketchup", source: "plant" },
+  { name: "bun", source: "plant" },
+  { name: "dessert twinkies", source: "unknown" },
+];
+
+console.log(isTheDinnerVegan(dinner));
+// Should print false

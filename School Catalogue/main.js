f0fcd046f6693cd9f3497af05663189d9d3c5853
yuhariermy <yuhariermy@gmail.com>
2022-06-10 14:33:30 +0700

    finish schoolCatalogue

--- School Catalogue/main.js
+++ School Catalogue/main.js
@@ -1,8 +1,8 @@
 class School {
-  constructor(name) {
+  constructor(name, level, numberOfStudents) {
     this._name = name;
-    this._level = "primary";
-    this._numberOfStudents = 50;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
   }
   get name() {
     return this._name;
@@ -14,9 +14,62 @@ class School {
     return this._numberOfStudents;
   }
 
-  set numberOfStudents(num) {}
+  set numberOfStudents(num) {
+    return num === "number"
+      ? this.numberOfStudents
+      : "Invalid input: NumberOfStudents must be set to a Number";
+  }
+  quickFacts() {
+    return `${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level`;
+  }
 
-  quickFacts() {}
-
-  static pickSubstituteTeacher() {}
+  static pickSubstituteTeacher(substituteTeachers) {
+    let randNum = Math.floor(Math.random() * substituteTeachers.length);
+    return substituteTeachers[randNum];
+  }
 }
+
+class PrimarySchool extends School {
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, "primary", numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
+}
+
+class HighSchool extends School {
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, "high", numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+  get sportsTeams() {
+    return this._sportsTeams;
+  }
+}
+
+const lorraineHansbury = new PrimarySchool(
+  "Lorraine Hansbury",
+  514,
+  "Students must be picked up by a parent, guardian, or a family member over the age of 13."
+);
+
+console.log(lorraineHansbury.quickFacts());
+School.pickSubstituteTeacher([
+  "Jamal Crawford",
+  "Lou Williams",
+  "J. R. Smith",
+  "James Harden",
+  "Jason Terry",
+  "Manu Ginobli",
+]);
+
+const alSmith = new HighSchool("Al E. Smith", 415, [
+  "Baseball",
+  "Basketball",
+  "Volleyball",
+  "Track and Field",
+]);
+
+console.log(alSmith.sportsTeams);
